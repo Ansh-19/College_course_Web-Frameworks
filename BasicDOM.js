8cecff00df64ed39d4d55f87b6461ce28b9332de
Ansh Mathur <80451133+Ansh-19@users.noreply.github.com>
2021-12-10 20:52:40 +0530

    Add files via upload

---
+++ BasicDOM.js
@@ -0,0 +1,18 @@
+//document.getElementById("mainheading").innerText="NCU";
+/*document.getElementbyId("para").innerText="NCU";*/
+function changeText(){
+    p=document.getElementById("mainheading");
+    //p.innerText=p.innerText + "NCU";
+    p.innerHTML="<h1>main Heading</h1>";
+    l = document.getElementsByTagName("label")[0]; 
+    l.innerHTML="This is the label"
+    //r=document.getElementsByName("radioinput");
+    
+    //console.log(r.value());
+    in=document.getElementById("inputbox");
+    console.log(i.value);
+    document.getElementById("inputbox").value=document.getElementById
+    ("inputbox").value + "Guragaon"; 
+}
+
+
